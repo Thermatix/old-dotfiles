b0894c74f7b3e60f09d570a3542418cba0a22f57
Martin Becker <mbeckerwork@gmail.com>
2017-10-06 09:40:59 +0100

    adds haml plugin for vim

--- tag-zsh/zsh/custom/functions.sh
+++ tag-zsh/zsh/custom/functions.sh
@@ -51,3 +51,16 @@ all-panes-bg_()
   tmux select-pane -t $ORIG_PANE_INDEX
 }
 fi
+
+
+notes() {
+  if [ ! -z "$1" ]; then
+    # Using the "$@" here will take all parameters passed into
+    # this function so we can place everything into our file.
+    echo "$@" >> "$HOME/notes.md"
+  else
+    # If no arguments were passed we will take stdin and place
+    # it into our notes instead.
+    cat - >> "$HOME/notes.md"
+  fi
+}
